ef77db1d853f80939bb3d9e518a000b941514a6e
Christopher Enam Franklin <cef1911@hotmail.com>
2023-04-18 19:28:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,56 +5,54 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import time
+# import pyplot as plt
 
 #import plotly as px
 #import plotly.figure_factory as ff
 
 
 """
-# Welcome to Chris Franklin's StreamLit Web App for Deploying DataScience Projects!
-This is inspired from the StreamLit.io site.
-See the website for more info https://streamlit.io/
-My name is Chris Franklin. I work in data science on several data science projects.  
-I have completed projects in web-scraping, sentiment analysis, time-series, nlp, robotics with python and data visualizations of all types. 
-I also cheerfully volunteer doing data science mentoring. 
-Streamlit web app generated through GitHub repo: 
-https://cef1911-chris-f-streamlit-datascience-app-stream-app-5p099v.streamlit.app/
-My primary programming language for data science projects is Python. 
-My Applied Data Science Lab Website is accessible at this url:
-https://franklydata.wixsite.com/franklydata
-Below is my Twitter Web-Scraping Sentiment Analysis Project using Google Colab. Run the code by signing into a gmail account. 
-I will be deploying my Twitter Sentiment Analysis Project on Streamlit in the future :)
-https://colab.research.google.com/drive/16ILWejhcam279Aetc8QGanhyeuSPIbQA
-StreamLit.io content example from their website
-StreamLit Documentation below:
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Demo of streamlit components Kids Fishing Data Science Project
+DataFrames which will be the team info.
+4 teams of 5 Kids
+More info to be added consistently
 """
 
+df2 = pd.DataFrame(
+    [
+        {"Team A": "John", "Role": "Captain", "Fish Caught": 21},
+        {"Team A": "Sara", "Role": "Data Wrangler", "Fish Caught": 4},
+        {"Team A": "Tony", "Role": "Team Liason", "Fish Caught": 10},
+        {"Team A": "Patricia", "Role": "Assistant Captain", "Fish Caught": 6},
+        {"Team A": "Tina", "Role": "Statistics Person", "Fish Caught": 30},
+    ]
+)
+
+
+st.dataframe(df2, use_container_width=True)
+
+st.bar_chart(df2, y = 'Team A', x='Fish Caught')
+
+# Crypto monthly data
+dc = {'Fish Caught':[1,2,3,4,5,6,7,8,9,10,11],
+     'Fishing Casts':[477,387,444,462,384,297,192,327,221,193,204]}
+     #'Ethereum':[3767,2796,2973,3448,2824,1816,1057,1630,1587,1311,1579]}
+
+fishdata = pd.DataFrame(data = dc)
+st.bar_chart(fishdata, y = 'Fish Caught', x='Fishing Casts')
+
+"""
+# Demo of Chart Data DataFrame
+"""
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=["a", "b", "c"])
 
 st.bar_chart(chart_data)
 
-# # Add histogram data
-# x1 = np.random.randn(200) - 2
-# x2 = np.random.randn(200)
-# x3 = np.random.randn(200) + 2
-
-# # Group data together
-# hist_data = [x1, x2, x3]
-
-# group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# # Create distplot with custom bin_size
-# fig = ff.create_distplot(
-#         hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# # Plot!
-# st.plotly_chart(fig, use_container_width=True)
+"""
+#Demo of Slider
+"""
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
@@ -76,10 +74,79 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
-    
+"""
+# Demo of Coin Flip
+Credit to:
+Nick Antonaccio (nick@com-pute.com)
+"""
+import streamlit as st, random, time    
+
+time.sleep(1)
+coins = ['http://re-bol.com/heads.jpg', 'http://re-bol.com/tails.jpg']
+coin = random.choice(coins)
+st.image(coin)
+if st.button('Flip'):
+  st.experimental_rerun()
+
+"""
+# Demo of Live Webcam Viewer
+Credit to:
+Nick Antonaccio (nick@com-pute.com)
+"""
+import streamlit as st
+
+cam=st.selectbox(
+  'Choose a Cam', 
+  [
+    '',
+    'pitriverbridge/pitriverbridge.jpg',
+    'johnsongrade/johnsongrade.jpg',
+    'perez/perez.jpg',
+    'mthebron/mthebron.jpg',
+    'eurekaway/eurekaway.jpg',
+    'sr70us395/sr70us395.jpg',
+    'bogard/bogard.jpg',
+    'eastriverside/eastriverside.jpg',
+  ]
+)
+if cam:
+  st.image('https://cwwp2.dot.ca.gov/data/d2/cctv/image/' + cam)
+
+"""
+# Demo of Calculator
+Credit to:
+Nick Antonaccio (nick@com-pute.com)
+"""
+import streamlit as st, ast
+
+if 'total' not in st.session_state:
+  st.session_state.total=''
+if st.button('Clear'): st.session_state.total=''
+col1, col2, col3, col4, col5=st.columns([1,1,1,1,4])
+if col1.button('1'): st.session_state.total+='1'
+if col2.button('2'): st.session_state.total+='2'
+if col3.button('3'): st.session_state.total+='3'
+if col4.button('+'): st.session_state.total+='+'
+if col1.button('4'): st.session_state.total+='4'
+if col2.button('5'): st.session_state.total+='5'
+if col3.button('6'): st.session_state.total+='6'
+if col4.button('-'): st.session_state.total+='-'
+if col1.button('7'): st.session_state.total+='7'
+if col2.button('8'): st.session_state.total+='8'
+if col3.button('9'): st.session_state.total+='9'
+if col4.button('.'): st.session_state.total+='.'
+if col1.button('0'): st.session_state.total+='0'
+if col2.button('*'): st.session_state.total+='*'
+if col3.button('/'): st.session_state.total+='/'
+if col4.button('='): 
+  st.session_state.total=str(eval(st.session_state.total))
+st.text_input('Result', st.session_state.total)
+
 st.write("Streamlit Play:) creating dataframes and plotly plots")
 
-
+"""
+# Demo of Schedule Slider
+"""
 
 from datetime import time
 
@@ -87,65 +154,3 @@ appointment = st.slider(
     "Schedule your appointment:",
     value=(time(11, 30), time(12, 45)))
 st.write("You're scheduled for:", appointment)
-
-
-title = st.text_input('Movie title', 'Life of Brian')
-st.write('The current movie title is', title)
-
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
-
-
-
-with st.form("my_form"):
-   st.write("Inside the form")
-   slider_val = st.slider("Form slider")
-   checkbox_val = st.checkbox("Form checkbox")
-
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val)
-
-st.write("Outside the form")
-
-"""Plotting a Map with Streamlit, documentation code is from this url: 
-https://docs.streamlit.io/library/get-started/main-concepts"""
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
-
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
-
-    chart_data
-     
-st.text_input("Your name", key="name")
-
-# You can access the value at any point with:
-st.session_state.name
-
-
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
-
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected: ', option
-
-st.balloons()
-
-st.snow()
