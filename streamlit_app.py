961d067dd8736be5d2a077d81b4ebed94a49ec6b
Christopher Enam Franklin <cef1911@hotmail.com>
2023-04-21 09:28:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ DataFrames which will be the team info.
 4 teams of 5 Kids
 More info to be added consistently
 
-Hello Stacey and Mike and Affiz
+# Hello Stacey and Mike and Affiz
 """
 
 df2 = pd.DataFrame(
